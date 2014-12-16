4ef8e469b7726a69fa8e0ce986a013893b05eb31
ndrewv <ndrewv@y7mail.com>
2014-12-16 15:03:53 +1100

    added javascript and html

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,36 @@
+var initialised = false;
+var uri = 'https://github.com/ndrewv/text-watch-configurable/texttime.html';
+
+function appMessageAck(e) {
+    console.log("options sent to Pebble successfully");
+}
+
+function appMessageNack(e) {
+    console.log("options not sent to Pebble: " + e.error.message);
+}
+
+Pebble.addEventListener("ready", function() {
+    initialised = true;
+});
+
+Pebble.addEventListener("showConfiguration", function() {
+    var config = JSON.parse(localStorage.getItem('config'));
+    console.log("read config: " + JSON.stringify(config));
+    console.log("showing configuration");
+    if (options !== null) {
+        uri = uri+ '?' + 'background=' + encodeURIComponent(config.background);
+    }
+    Pebble.openURL(uri);
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+    console.log("configuration closed");
+    if (e.response !== '') {
+        var options = JSON.parse(decodeURIComponent(e.response));
+        console.log("storing config: " + JSON.stringify(config));
+        localStorage.setItem('config', JSON.stringify(config));
+        Pebble.sendAppMessage(config, appMessageAck, appMessageNack);
+    } else {
+        console.log("no config received");
+    }
+});
